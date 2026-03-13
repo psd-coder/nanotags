2cb18c51503a646956e6912329d18baecee89627
Pavel Grinchenko <psd-coder@evilmartians.com>
2026-03-13 18:03:26 +0000

    feat(docs): add Layout, Footer, Icon, TableOfContents components and site config

---
+++ docs/src/config.ts
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  name: "nano-wc",
+  author: {
+    name: "Pavel Grinchenko",
+    twitter: "https://x.com/psd_coder",
+  },
+  github: "https://github.com//psd-coder/nano-wc",
+  license: {
+    name: "MIT",
+    url: "https://github.com//psd-coder/nano-wc/blob/main/LICENSE",
+  },
+  evilMartians: {
+    name: "Evil Martians",
+    url: "https://evilmartians.com/",
+  },
+};
